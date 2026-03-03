2be99a332644ad429c353846c71bea80468e75ca
Adin Mujakovic <adinmujakovic@outlook.com>
2026-03-02 22:08:29 -0800

    Add Assignment 7.1 that showcases filtering and secondary pages with MVC. It uses the Olympics as a example.

---
+++ Controllers/OlympicController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhoneWebApp.Models;
+
+namespace PhoneWebApp.Controllers
+{
+    public class OlympicController : Controller
+    {
+        public IActionResult Index(string game, string category)
+        {
+            var model = OlympicRepository.GetCountries();
+
+            if (!string.IsNullOrEmpty(game) && game != "ALL")
+            {
+                model = model.Where(x => x.Game == game).ToList();
+            }
+            if (!string.IsNullOrEmpty(game) && category != "ALL")
+            {
+                model = model.Where(x => x.Category == category).ToList();
+            }
+            return View(model);
+        }
+        public IActionResult Details(string id)
+        {
+            var country = OlympicRepository
+                .GetCountries()
+                .FirstOrDefault(x => x.Country == id);
+
+            return View(country);
+        }
+    }
+}
