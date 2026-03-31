64d92fa24c8ea71428fd7ee849ae625aa3bbdebc
Adin Mujakovic <adinmujakovic@outlook.com>
2026-03-30 21:29:03 -0700

    Add validation for the tickets to assignment 10.1 Todo List.

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PhoneWebApp.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhoneWebApp.Controllers
 {
@@ -9,7 +10,8 @@ namespace PhoneWebApp.Controllers
         {
             new Ticket { Id=1, Name="Create Base Page", Description="Basic layout", SprintNumber=1, PointValue=3, Status="To Do"},
             new Ticket { Id=2, Name="Add Decoration", Description="Decoration", SprintNumber=1, PointValue=5, Status="In Progress"},
-            new Ticket { Id=3, Name="Testing", Description="QA checks", SprintNumber=2, PointValue=2, Status="QA"}
+            new Ticket { Id=3, Name="Testing", Description="QA checks", SprintNumber=2, PointValue=2, Status="QA"},
+            new Ticket { Id=4, Name="Testing", Description="QA checks", SprintNumber=55, PointValue=23, Status="QA"}
         };
 
         public IActionResult Index(string status = "All")
@@ -21,7 +23,33 @@ namespace PhoneWebApp.Controllers
             vm.Tickets = status == "All"
                 ? tickets
                 : tickets.Where(t => t.Status == status).ToList();
+            List<Ticket> validTickets = new List<Ticket>();
+            // VALIDATE EACH TICKET
+            foreach (var ticket in vm.Tickets)
+            {
+                var context = new ValidationContext(ticket);
+                var results = new List<ValidationResult>();
 
+                bool isValid = Validator.TryValidateObject(ticket, context, results, true);
+
+                if (isValid)
+                {
+                    validTickets.Add(ticket);
+                }
+                else
+                {
+                    foreach (var error in results)
+                    {
+                        ModelState.AddModelError("",
+                            $"Ticket '{ticket.Name}' with ID: {ticket.Id}: {error.ErrorMessage}. Ticket was not displayed");
+
+                    }
+
+
+                }
+               
+            }
+            vm.Tickets = validTickets;
             return View(vm);
         }
     }
