19b556518267e92b9e2f0110245858375c1e7619
Adin Mujakovic <adinmujakovic@outlook.com>
2026-04-21 20:48:41 -0700

    Add tag helper + viwe components to assignment 10.1 Todo List

---
+++ ViewComponents/StatusViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneWebApp.ViewComponents
+{
+    public class StatusViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string status)
+        {
+            return View("Default", status);
+        }
+    }
+}
